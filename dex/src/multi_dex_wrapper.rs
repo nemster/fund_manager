ed17b2653dc070dc63558eda940336bd48cf03c7
Marco Michelino <marco.michelino@immobiliare.it>
2025-08-22 21:32:06 +0200

    internal documantation + optimizations in swap method

--- dex/src/multi_dex_wrapper.rs
+++ dex/src/multi_dex_wrapper.rs
@@ -2,15 +2,19 @@ use scrypto::prelude::*;
 use crate::common::*;
 use scrypto_interface::*;
 
+// Fail the transaction if more than 1 / this ratio of the source coin has been returned by
+// the dex
 static ACCEPTABLE_REMAININGS_RATIO: u8 = 10;
 
-#[derive(ScryptoSbor)]
+// Couple of coins managed by a dex
+#[derive(ScryptoSbor, Debug)]
 struct CoinsCouple {
     from: ResourceAddress,
     to: ResourceAddress,
 }
 
-#[derive(ScryptoSbor, Clone)]
+// Type of dex
+#[derive(ScryptoSbor, Clone, Debug)]
 enum PoolType {
     OciswapPool2,
     OciswapPrecisionPool,
@@ -18,12 +22,17 @@ enum PoolType {
     DefiPlazaPool,
 }
 
-#[derive(ScryptoSbor, Clone)]
+// Dex information
+#[derive(ScryptoSbor, Clone, Debug)]
 struct DexPool {
     pool_type: PoolType,
     component: Global<AnyComponent>,
 }
 
+// This blueprint wraps any number of pools in a single inteface; it can perform multi steps swaps
+// involving different pool types.
+// 
+// Only one pool for ordered coin couple is supported (a->b pool can be different from b->a pool).
 #[blueprint_with_traits]
 #[types(
     CoinsCouple,
@@ -32,36 +41,6 @@ struct DexPool {
     Vault,
 )]
 mod multi_dex_wrapper {
-
-/*
-    extern_blueprint! {
-        "package_tdx_2_1p5qntnqluczzjjnm577mfp7p5jd3qm2sv0qzkqklgkrypcnspw3dff",
-        Pool {
-            fn swap(&mut self, input_bucket: Bucket) -> Bucket;
-        }
-    }
-
-    extern_blueprint! {
-        "package_tdx_2_1pha4sgrwdc09ymqy6e8gpe7h0e652p5l22esxd2t8u82290cjq45ma",
-        PrecisionPool {
-            fn swap(&mut self, input_bucket: Bucket) -> (Bucket, Bucket); // (output_bucket, remainings)
-        }
-    }
-
-    extern_blueprint! {
-        "package_tdx_2_1p4g09xagmsyql6r65a70c94n6qgvk6ffx9q0z5g3vnqmrsr96627vg",
-        QuantaSwap {
-            fn swap(&mut self, tokens: Bucket) -> (Bucket, Bucket); // (output_bucket, remainings)
-        }
-    }
-
-    extern_blueprint! {
-        "package_tdx_2_1pk8umega5zc4fft0lmemml3g6qlce99h3wszsukwe3lx45xqay6l9t",
-        PlazaPair {
-            fn swap(&mut self, input_bucket: Bucket) -> (Bucket, Option<Bucket>);
-        }
-    }
-*/
 
     enable_method_auth! {
         roles {
@@ -74,16 +53,19 @@ mod multi_dex_wrapper {
     }
 
     struct MultiDexWrapper {
-        pools: KeyValueStore<CoinsCouple, DexPool>,
-        remainings: KeyValueStore<ResourceAddress, Vault>,
+        pools: KeyValueStore<CoinsCouple, DexPool>,         // All known pools
+        remainings: KeyValueStore<ResourceAddress, Vault>,  // Vaults to store eventual unswapped coins
     }
 
     impl MultiDexWrapper {
 
+        // Instantiate and globalize a MultiDexWrapper component
         pub fn new(
-            fund_manager_badge_address: ResourceAddress,
-            admin_badge_address: ResourceAddress,
+            fund_manager_badge_address: ResourceAddress,    // FundManager badge
+            admin_badge_address: ResourceAddress,           // Owner badge
         ) -> Global<MultiDexWrapper> {
+
+            // Instantiate and globalize the component
             Self {
                 pools: KeyValueStore::new_with_registered_type(),
                 remainings: KeyValueStore::new_with_registered_type(),
@@ -96,14 +78,18 @@ mod multi_dex_wrapper {
                 .globalize()
         }
 
+        // Add the pool to swap from "from" to "to" coins
         pub fn add_pool(
             &mut self,
-            from: ResourceAddress,
-            to: ResourceAddress,
-            pool_type: String,
-            component: Global<AnyComponent>,
-            opposite_too: bool,
+            from: ResourceAddress,              // Input coin
+            to: ResourceAddress,                // Output coin
+            pool_type: String,                  // Pool type
+            component: Global<AnyComponent>,    // Pool component address
+            opposite_too: bool,                 // Whether to use the same pool for the opposite
+                                                // swaps too
         ) {
+
+            // Build the DexPool object
             let pool = match pool_type.as_str() {
                 "ociswap_pool2" => DexPool {
                     pool_type: PoolType::OciswapPool2,
@@ -124,6 +110,7 @@ mod multi_dex_wrapper {
                 _ => { Runtime::panic("Unrecognized pool type".to_string()); },
             };
 
+            // Insert the DexPool object in the pools KVS
             self.pools.insert(
                 CoinsCouple {
                     from: from,
@@ -132,6 +119,8 @@ mod multi_dex_wrapper {
                 pool.clone()
             );
 
+            // If required insert a clone of the DexPool object in the KVS for the opposite swaps
+            // too
             if opposite_too {
                 self.pools.insert(
                     CoinsCouple {
@@ -142,100 +131,139 @@ mod multi_dex_wrapper {
                 );
             }
         }
+
+        // Private method to withdraw from the remainings vaults
+        fn take_from_remainings(
+            &mut self,
+            resource: ResourceAddress,
+            amount: Decimal,
+        ) -> Option<Bucket> {
+
+            // Get the remainings vault for the given resource; return None if not found
+            let mut vault = self.remainings.get_mut(&resource);
+            if vault.is_none() {
+                return None;
+            }
+
+            // If amount is bigger than the available amount get the full available amount from
+            // the Vault
+            let available_amount = vault.as_mut().unwrap().amount();
+            if available_amount < amount {
+                return Some(vault.unwrap().take_all());
+            }
+
+            // otherwise pay attention to divisibility
+            return Some(
+                vault.unwrap().take_advanced(
+                    amount,
+                    WithdrawStrategy::Rounded(RoundingMode::ToZero)
+                )
+            );
+        }
     }
 
     impl DexInterfaceTrait for MultiDexWrapper {
 
+        // This method swaps the input_bucket for the output_resource
         fn swap(
             &mut self,
-            mut input_bucket: Bucket,
-            output_resource: ResourceAddress,
+            mut input_bucket: Bucket,           // Bucket of coins to swap
+            output_resource: ResourceAddress,   // Output coins resource address
         ) -> Bucket {
             let input_bucket_amount = input_bucket.amount();
             let input_resource = input_bucket.resource_address();
 
-            match self.remainings.get_mut(&input_resource) {
-                Some(mut vault) =>
-                    if vault.amount() > Decimal::ZERO {
-                        input_bucket.put(vault.take_all());
-                    },
-                None => {},
+            // If there are remainings of the input resource from the previous swaps, add (part of)
+            // them to the input_bucket
+            let remainings_bucket = self.take_from_remainings(
+                input_resource,
+                input_bucket_amount / ACCEPTABLE_REMAININGS_RATIO,
+            );
+            if remainings_bucket.is_some() {
+                input_bucket.put(remainings_bucket.unwrap());
             }
 
+            // Find the dex pool to use for the swap
             let coins_couple = CoinsCouple {
                 from: input_resource,
                 to: output_resource,
             };
+            let pool = self.pools.get_mut(&coins_couple);
 
             let output_bucket: Bucket;
-
-            let pool = self.pools.get_mut(&coins_couple);
+            let mut remainings_bucket: Option<Bucket> = None;
 
             match pool {
                 Some(dex_pool) => {
                     match dex_pool.pool_type {
+
+                        // Ociswap latest pools only returns the output_bucket, no remainings
                         PoolType::OciswapPool2 => {
                             output_bucket = dex_pool.component
                                 .call::<Bucket, Bucket>(
                                     "swap",
                                     &input_bucket
                                 );
-
-                            input_bucket = Bucket::new(input_resource);
                         },
+
+                        // DefiPlaza return the output bucket and eventually a remainings bucket
                         PoolType::DefiPlazaPool => {
-                            let (output, opt_input) = dex_pool.component
+                            (output_bucket, remainings_bucket) = dex_pool.component
                                 .call::<Bucket, (Bucket, Option<Bucket>)>(
                                     "swap",
                                     &input_bucket
                                 );
-
-                            output_bucket = output;
-
-                            match opt_input {
-                                None => input_bucket = Bucket::new(input_resource),
-                                Some(bucket) => input_bucket = bucket,
-                            }
                         },
-                        _ => (output_bucket, input_bucket) = dex_pool.component
-                            .call::<Bucket, (Bucket, Bucket)>(
-                                "swap",
-                                &input_bucket
-                            ),
+
+                        // Both Caviarnine pools and Ociswap precision pools always return a couple
+                        // of buckets
+                        _ => {
+                            (output_bucket, input_bucket) = dex_pool.component
+                                .call::<Bucket, (Bucket, Bucket)>(
+                                    "swap",
+                                    &input_bucket
+                                );
+
+                            remainings_bucket = Some(input_bucket);
+                        },
                     }
                 },
+
+                // If there's no direct input -> output pool try doing input -> XRD, XRD -> output
                 None => {
                     if input_resource != XRD && output_resource != XRD {
                         drop(pool);
 
+                        // Call recursivelly twice this method for the 2 phase swap
                         let xrd_bucket = self.swap(input_bucket, XRD);
-
                         output_bucket = self.swap(xrd_bucket, output_resource);
 
-                        input_bucket = Bucket::new(input_resource);
                     } else {
                         Runtime::panic("Pool not found".to_string());
                     }
                 },
             };
 
-            assert!(
-                input_bucket.amount() < input_bucket_amount / ACCEPTABLE_REMAININGS_RATIO,
-                "Swap failed",
-            );
+            if remainings_bucket.is_some() {
+                // Check if there are too much remainings
+                assert!(
+                    remainings_bucket.as_ref().unwrap().amount() < input_bucket_amount / ACCEPTABLE_REMAININGS_RATIO,
+                    "Swap failed",
+                );
 
-            let remainings_vault = self.remainings.get_mut(&input_resource);
+                // Put remainings in the vault 
+                let remainings_vault = self.remainings.get_mut(&input_resource);
+                match remainings_vault {
+                    Some(mut vault) => vault.put(remainings_bucket.unwrap()),
+                    None => {
+                        drop(remainings_vault);
 
-            match remainings_vault {
-                Some(mut vault) => vault.put(input_bucket),
-                None => {
-                    drop(remainings_vault);
-
-                    self.remainings.insert(
-                        input_resource,
-                        Vault::with_bucket(input_bucket)
-                    );
-                },
+                        self.remainings.insert(
+                            input_resource,
+                            Vault::with_bucket(remainings_bucket.unwrap())
+                        );
+                    },
+                }
             }
 
             output_bucket
